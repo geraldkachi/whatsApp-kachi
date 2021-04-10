e8f2e5c2afeb49a2013c100105763e0617635525
geraldkachi <fitzgeraldkachi@gmail.com>
2021-04-10 17:00:33 +0100

    adding shadow to the bottom tab

--- screens/BottomTabScreen.js
+++ screens/BottomTabScreen.js
@@ -119,7 +119,18 @@ const BottomTabScreen = () => (
 
 export default BottomTabScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor:"#7F5DF0",
+    shadowOffset: {
+        width: 0,
+        height: 10
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
+}
+});
 
 
 
